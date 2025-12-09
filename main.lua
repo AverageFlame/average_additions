852b7c578efcde38187f16ba6f76ea707d5c53d6
jumpsplat120 <jumpsplat120@yahoo.com>
2025-12-08 19:38:56 -0800

    Simplify game resetting
    initialize/cleanup variable functions were only ever called in one place; the function directly above them. Move logic into function directly to reduce cognitive overhead.
    Original init/cleanup functions had a lot of overlap in behaviour. Initalize would create tables that cleanup also created, or cleanup wouldn't create something, only for init to end up doing it. These two processes are effectively one in the same, and so merging them allows us to reduce complexity and line count.
    Use early exit to reduce indentation.
    Rewrite for loop to be less wordy. There was a lot of unneeded checking before acting. Using `or` lets us sidestep a lot of that.

--- main.lua
+++ main.lua
@@ -7,67 +7,31 @@ function noop() end
 
 AVE = {}
 
-function SMODS.current_mod.reset_game_globals(run_start)
-    if run_start then
-		ave_cleanup_variables()
-        ave_initialize_variables()
-	end
-end
+function SMODS.current_mod.reset_game_globals(start)
+    if not start then return end
 
--- Function to initialize AVE mod variables for a new run
-function ave_initialize_variables()
-    -- Initialize core AVE structure
-    AVE.MAP = AVE.MAP or { paths = {}, levels = {} }
+    AVE.MAP = AVE.MAP or {}
+
+    -- Remove any UI elements that might still exist.
+    for x = 1, #(AVE.MAP.cell_icon or {}), 1 do
+        for y = 1, #(AVE.MAP.cell_icon[x] or {}), 1 do
+            (AVE.MAP.cell_icon[x][y] or { remove = noop }):remove()
+        end
+    end
+
+    --Remove map (map vs MAP?)
+    (AVE.map or { remove = noop }):remove()
+
+    AVE.MAP.paths             = {}
+    AVE.MAP.levels            = {}
+    AVE.MAP.cell_icon         = {}
+    AVE.MAP.current_level     = {}
     AVE.MAP.selectable_levels = {}
+    
+    AVE.rarity_weights = {}
+    
+    AVE.MAP.limit         = nil
     AVE.MAP.current_stage = nil
-    AVE.MAP.current_level = {}
-    AVE.MAP.limit = nil
-    
-    AVE.rarity = nil
-    AVE.rarity_weights = {}
-end
-
--- Function to clean up all AVE mod variables
-function ave_cleanup_variables()
-    -- Clean up map-related variables
-    if AVE.MAP then
-        if AVE.MAP.paths then
-            AVE.MAP.paths = {}
-        end
-        if AVE.MAP.levels then
-            AVE.MAP.levels = {}
-        end
-        if AVE.MAP.selectable_levels then
-            AVE.MAP.selectable_levels = {}
-        end
-        if AVE.MAP.cell_icon then
-            -- Remove any UI elements that might still exist
-            for i = 1, #AVE.MAP.cell_icon do
-                if AVE.MAP.cell_icon[i] then
-                    for j = 1, #AVE.MAP.cell_icon[i] do
-                        if AVE.MAP.cell_icon[i][j] and AVE.MAP.cell_icon[i][j].remove then
-                            AVE.MAP.cell_icon[i][j]:remove()
-                        end
-                    end
-                end
-            end
-            AVE.MAP.cell_icon = {}
-        end
-        AVE.MAP.current_stage = nil
-        AVE.MAP.current_level = {}
-        AVE.MAP.limit = nil
-    end
-    
-    -- Clean up other AVE variables
-    AVE.rarity = nil
-    AVE.rarity_weights = {}
-    
-    -- Clean up the map UI if it exists
-    if AVE.map then
-        AVE.map:remove()
-        AVE.map = nil
-    end
-end
 
 local helper, load_error = SMODS.load_file("map_UI.lua")
 if load_error then
@@ -92,6 +56,8 @@ if load_error then
   sendDebugMessage ("The error is: "..load_error)
 else
   helper()
+    AVE.map    = nil
+    AVE.rarity = nil
 end
 
 
