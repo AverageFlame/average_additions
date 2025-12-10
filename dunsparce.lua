735da69d033f453efe682d944398488ec6be2873
jumpsplat120 <jumpsplat120@yahoo.com>
2025-12-10 14:18:12 -0800

    Counter race condition
    find_card isn't accurate when called within add_to_deck. By keeping track of how many dunsparces the player has ourselves, we no longer encounter that issue.

--- dunsparce.lua
+++ dunsparce.lua
@@ -1,5 +1,5 @@
 local TextNode, RectNode, ObjectNode
-local colors
+local colors, dunsparces
 
 --Creating variables both makes it easier to understand (since
 --localthunk is afraid of words) and is a microoptimization,
@@ -14,6 +14,8 @@ colors = G.C
 
 Ave_color = colors.GREEN
 Dunsparce = Sprite(0,0, 0.5, 0.5, G.ASSET_ATLAS["angry-dunsparce"])
+
+dunsparces = 0
 
 --Creates either the red button or green button. Pass in the color, and
 --the nodes that go on the top part. The bottom is always the reroll cost.
@@ -108,10 +110,10 @@ function createButton(color, ...)
     }
 end
 
+--If the player has dunsparce in their deck, then this will change the reroll
+--option to be a dunsparce reroll. Otherwise, it'll be a regular reroll. 
 function ave_check_dunsparce()
-    local red, green, has_card, reroll_button, nodes
-
-    has_card = #SMODS.find_card("j_poke_dunsparce") > 0
+    local red, green, reroll_button, nodes
 
     Dunsparce = Sprite(0,0, 0.5, 0.5, G.ASSET_ATLAS["angry-dunsparce"])
 
@@ -149,7 +151,21 @@ function ave_check_dunsparce()
 
     reroll_button:remove()
 
-    G.shop:add_child(has_card and red or green, reroll_button)
+    G.shop:add_child(dunsparces > 0 and red or green, reroll_button)
 
-    Ave_color = has_card and colors.RED or colors.GREEN
+    Ave_color = dunsparces > 0 and colors.RED or colors.GREEN
+end
+
+--Keep track of dunsparces in deck. Prevents race conditions with `find_in_deck`.
+--To be used as a callback function.
+function addDunsparceToDeck()
+    dunsparces = dunsparces + 1
+
+    ave_check_dunsparce()
+end
+
+function removeDunsparceFromDeck()
+    dunsparces = dunsparces - 1
+
+    ave_check_dunsparce()
 end
